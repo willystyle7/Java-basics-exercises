69b82277ded1d7138aa9ab30a27c7ea99a5a2a18
willystyle7 <ilia111@yahoo.com>
2018-06-12 18:53:32 +0300

    problems from Telerik

---
+++ Telerik/FirstControl/01.SendMeTheCode.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class SendMeTheCode
+{
+    static void Main()
+    {
+        string number = Math.Abs(int.Parse(Console.ReadLine())).ToString();
+        int result = 0;
+        for (int i = 0; i < number.Length; i++)
+        {
+            int index = number.Length - i;
+            int digit = number[i] - '0';
+            if (index % 2 == 0)
+            {
+                result += digit * digit * index;
+            }
+            else
+            {
+                result += digit * index * index;
+            }
+        }
+        int lengthMessage = result % 10;
+        int startMessage = result % 26 + 1;
+        if (lengthMessage == 0)
+        {
+            Console.WriteLine(result);
+            Console.WriteLine("Big Vik wins again!");
+        }
+        else
+        {
+            string resultMessage = "";
+            int indexMessage = startMessage;
+            for (int i = 1; i <= lengthMessage; i++)
+            {
+                resultMessage += (char)(64 + indexMessage);                
+                indexMessage = indexMessage % 26 + 1;
+            }
+            Console.WriteLine(result);
+            Console.WriteLine(resultMessage);
+        }
+
+    }
+}
+
---
+++ Telerik/FirstControl/04.ExpressionAgain.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class ExpressionAgain
+{
+    static void Main()
+    {
+        char[] digits = Console.ReadLine().ToCharArray();
+        int controlExp = int.Parse(Console.ReadLine());
+        int counter = 0;
+        Queue<string> expressions = new Queue<string>();        
+        expressions.Enqueue(digits[0].ToString());
+        for (int i = 1; i < digits.Length; i++)
+        {
+            int tempCount = expressions.Count;
+            for (int j = 0; j < tempCount; j++)
+            {
+                string exp = expressions.Dequeue();
+                expressions.Enqueue(exp + "*" + digits[i].ToString());
+                expressions.Enqueue(exp + "+" + digits[i].ToString());
+                expressions.Enqueue(exp + "-" + digits[i].ToString());
+                if (digits[i - 1] != '0')
+                {
+                    expressions.Enqueue(exp + "" + digits[i].ToString());
+                }
+                else if (exp.Length >= 2 && exp[exp.Length - 2] != '*' && exp[exp.Length - 2] != '+' && exp[exp.Length - 2] != '-')
+                {
+                    expressions.Enqueue(exp + "" + digits[i].ToString());
+                }                
+            }
+        }
+        while (expressions.Count > 0)
+        {
+            string expression = expressions.Dequeue();
+            if (EvaluateExression(expression) == controlExp)
+            {
+                counter++;
+            }
+            //Console.WriteLine(expression);
+            //Console.WriteLine(EvaluateExression(expression));
+        }
+        Console.WriteLine(counter);
+    }
+
+    static int EvaluateExression(string expresion)
+    {
+        int sum = 0;
+        string patternNumbers = @"\d+";
+        string patternOperators = @"[+*-]{1}";
+        MatchCollection matchesNumbers = Regex.Matches(expresion, patternNumbers);
+        MatchCollection matchesOperators = Regex.Matches(expresion, patternOperators);
+        sum = int.Parse(matchesNumbers[0].Value);
+        for (int i = 1; i < matchesNumbers.Count; i++)
+        {
+            switch (matchesOperators[i - 1].Value)
+            {
+                case "+":
+                    sum += int.Parse(matchesNumbers[i].Value);
+                    break;
+                case "-":
+                    sum -= int.Parse(matchesNumbers[i].Value);
+                    break;
+                case "*":
+                    sum *= int.Parse(matchesNumbers[i].Value);
+                    break; 
+            }
+        }
+        return sum;
+    }
+}
+
